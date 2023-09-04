99b9c2d6848d98c80b27dd9b85ce9990ccc1fd3d
opticXC <aabishmalik018@gmail.com>
2023-09-05 00:29:21 +0530

    added default lsp configs for some things

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,39 @@
+local lsp = require("lsp-zero")
+local lspconfig = require("lspconfig")
+
+lsp.preset("recommended")
+
+lsp.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+
+lsp.ensure_installed({
+    'tsserver',
+    'rust_analyzer',
+    'denols',
+})
+
+lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
+
+lspconfig.denols.setup {
+    on_attach = on_attach,
+    root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+}
+
+lspconfig.tsserver.setup {
+  on_attach = on_attach,
+  root_dir = lspconfig.util.root_pattern("package.json"),
+  single_file_support = false
+}
+
+lsp.nvim_workspace()
+
+local cmp = require("cmp")
+
+
+
+
+lsp.setup()
--- lua/opticXC/lazy.lua
+++ lua/opticXC/lazy.lua
@@ -13,11 +13,29 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     "folke/which-key.nvim",
+    "folke/tokyonight.nvim",   
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.2',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
 
+
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        dependencies = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        },
+        
+    },
 }, opts)
 
 
