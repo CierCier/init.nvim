1e2d13345154e6e24247b376476262cde5e31002
opticXC <aabishmalik018@gmail.com>
2023-09-05 10:55:12 +0530

    added treesitter

--- lua/opticXC/lazy.lua
+++ lua/opticXC/lazy.lua
@@ -13,7 +13,15 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
     "folke/which-key.nvim",
-    "folke/tokyonight.nvim",   
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-treesitter/playground",
+    "mbbill/undotree",
+
+    {
+        'folke/tokyonight.nvim',
+        lazy=false,
+    },
+
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.2',
         dependencies = { 'nvim-lua/plenary.nvim' }
@@ -34,8 +42,9 @@ require("lazy").setup({
             {'hrsh7th/cmp-nvim-lsp'}, -- Required
             {'L3MON4D3/LuaSnip'},     -- Required
         },
-        
+
     },
+
 }, opts)
 
 
