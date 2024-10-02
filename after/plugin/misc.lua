2bda6a7c95b10fb1170e12f447f12ef755eef046
Aabish Malik <aabishmalik3337@gmail.com>
2024-10-02 19:43:15 +0530

    update plugins

--- after/plugin/misc.lua
+++ after/plugin/misc.lua
@@ -1,4 +1,8 @@
 
 
+local rmd = require("render-markdown")
 
+vim.keymap.set('n', '<leader>mt', function()
+    rmd.toggle()
+end)
 
--- lua/opticXC/lazy.lua
+++ lua/opticXC/lazy.lua
@@ -77,6 +77,24 @@ require("lazy").setup({
 		'cohama/lexima.vim',
 		event = { 'InsertEnter', 'CmdlineEnter' },
 		branch = 'master',
+	},
+	{
+		'MeanderingProgrammer/render-markdown.nvim',
+	    opts = {},
+		dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+	    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+	},
+	{
+	    "HakonHarnes/img-clip.nvim",
+	    event = "VeryLazy",
+	    opts = {
+		
+	    },
+
+	    keys = {
+		{ "<leader>pp", "<cmd>PasteImage<cr>", desc = "Paste Image from system clipboard"   }
+	    }
 	}
 }, {
     lazy = true,
