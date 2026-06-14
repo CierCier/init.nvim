41550eea55cea99ac486928bf44a1cb65c44e9a5
Aabish Malik <aabishmalik3337@gmail.com>
2026-06-14 14:07:51 +0530

    feat: switch to oxocarbon OLED theme
    - Replace rose-pine with oxocarbon.nvim (true black background)
    - Remove dankcolors/base16 custom theme + matugen watcher

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,7 +1,7 @@
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
+	"nyoom-engineering/oxocarbon.nvim",
+	priority = 1000,
 	config = function()
-		vim.cmd("colorscheme rose-pine")
-	end
+		vim.cmd("colorscheme oxocarbon")
+	end,
 }
